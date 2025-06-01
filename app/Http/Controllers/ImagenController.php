6162eaa5b056e5845d9cad804f24762491bc4fab
Sandro <sbarroscaballero0826@gmail.com>
2025-06-01 17:31:50 -0500

    Imagn Procsar

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -5,19 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ImagenController extends Controller
 {
     //
     public function store(Request $request)
     {
+        // Obtener la imagen
         $imagen = $request->file('file');
 
+        // Generar un nombre unico
         $nombreImagen = Str::uuid() . "." . $imagen->extension();
 
-        $imagenServidor = Image::make($imagen);
+        $manager = new ImageManager(new Driver());
 
-        $imagenServidor->fit(1000, 1000);
+        $imagenServidor = $manager::imagick()->read($imagen);
+        $imagenServidor->cover(1000, 1000);
+
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+
+        $imagenServidor->save($imagenPath);
 
         return response()->json(['imagen' => $nombreImagen]);
     }
